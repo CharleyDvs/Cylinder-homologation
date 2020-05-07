3eeabf26ceaa215fb8bb65794f6ef732ccba756c
Jaime Yerena <ing.carlosjaime@outlook.com>
2020-05-07 13:32:11 -0500

    Added comparison between threads and select type lists for the bore sizes

--- cg1.js
+++ cg1.js
@@ -1,16 +1,3 @@
 var d20 = [20, 8, 18, 35, 69, 106, 11, 11];
 var input = document.getElementById("bore");
 
-function CG1measure(){
-  for (i=1; 1<7; i++){
-    document.getElementsByClassName("cg1")[i].value = ;
-  }
-}
-
-var cyl1d = document.getElementById("bore").value;
-
-input.addEventListener("keypress", function(event){
-  if (cyl1d === 20 && event.keyCode === 13) {
-    CG1measure();
-  }
-});
--- script.js
+++ script.js
@@ -15,32 +15,44 @@ button.addEventListener("click", function(){
         }
 
         if(difference === 0){
-            marker1.classList.remove("blue");
             marker1.classList.remove("red");
             marker1.classList.remove("yellow");
             marker1.classList.add("green");
-            marker2.classList.remove("blue");
             marker2.classList.remove("red");
             marker2.classList.remove("yellow");
             marker2.classList.add("green");
         }else if(difference <= tolerate){
-            marker1.classList.remove("blue");
             marker1.classList.remove("green");
             marker1.classList.remove("red");
             marker1.classList.add("yellow");
-            marker2.classList.remove("blue");
             marker2.classList.remove("green");
             marker2.classList.remove("red");
             marker2.classList.add("yellow");
         }else  if(difference > tolerate){
-            marker1.classList.remove("blue");
             marker1.classList.remove("yellow");
             marker1.classList.remove("green");
             marker1.classList.add("red");
-            marker2.classList.remove("blue");
             marker2.classList.remove("yellow");
             marker2.classList.remove("green");
             marker2.classList.add("red");
+        }
+    }
+    for (j = 0; j<3; j++){
+        var thread1 = document.getElementsByClassName("thread1")[j].value;
+        var thread2 = document.getElementsByClassName("thread2")[j].value;
+        var tmark1 = document.getElementsByClassName("tMark1")[j];
+        var tmark2 = document.getElementsByClassName("tMark2")[j];
+
+        if (thread1 === thread2) {
+            tmark1.classList.remove("red");
+            tmark1.classList.add("green");
+            tmark2.classList.remove("red");
+            tmark2.classList.add("green");
+        } else {
+            tmark1.classList.remove("green");
+            tmark1.classList.add("red");
+            tmark2.classList.remove("green");
+            tmark2.classList.add("red");
         }
     }
 });
