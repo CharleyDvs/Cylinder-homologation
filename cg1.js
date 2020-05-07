1ee68db9390006a9ccbc2a0e44ff50af9dfcfdf4
Jaime Yerena <ing.carlosjaime@outlook.com>
2020-05-07 16:37:11 -0500

    Autofill function implemented,added CG1 measures for cylinder bore 20

--- cg1.js
+++ cg1.js
@@ -1,3 +1,16 @@
-var d20 = [20, 8, 18, 35, 69, 106, 11, 11];
-var input = document.getElementById("bore");
+/*var structure []*/
+var d20 = [8, 15.5, 18, 35, 69, 106, 11, 11];
+var thread20 = ["M8x1.25", '1/8"', "M5x0.8"]
+var limit = document.getElementsByClassName("smc").length;
 
+function autoFill(){
+  var input = document.getElementById("bore").value;
+  if (input === "20"){
+    for(i = 0; i < limit; i++){
+      document.getElementsByClassName("smc")[i].value = d20[i];
+    }
+    for(i = 0; i < 3; i++) {
+      document.getElementsByClassName("smcThread")[i].value = thread20[i];
+    }
+  }
+}
