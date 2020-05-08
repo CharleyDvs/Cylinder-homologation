e76c35ff9ae9110d4a4b1daae0af8fa27b679742
Jaime Yerena <ing.carlosjaime@outlook.com>
2020-05-08 13:12:14 -0500

    Autofill function optimized, full measures for CG1 added

--- cg1.js
+++ cg1.js
@@ -1,58 +1,38 @@
+var boreSizes = ["20", "25", "32", "40", "50", "63", "80", "100"];
 /*var structure [D, AL, A, H, S, ZZ, TA, TB]*/
-var d20 = [8, 15.5, 18, 35, 69, 106, 11, 11];
+var smcMeasures = [[8, 15.5, 18, 35, 69, 106, 11, 11],
+[10, 19.5, 22, 40, 69, 111, 11, 11],
+[12, 19.5, 22, 40, 71, 113, 11, 10],
+[16, 27, 30, 50, 78, 130, 12, 10],
+[20, 32, 35, 58, 90, 150, 13, 12],
+[20, 32, 35, 58, 90, 150, 13, 12],
+[25, 37, 40, 71, 108, 182],
+[30, 37, 40, 71, 108, 182]
+];
 /*var structure [MM, P, TC]*/
-var thread20 = ["M8x1.25", '1/8"', "M5x0.8"];
-var d25 = [10, 19.5, 22, 40, 69, 111, 11, 11];
-var thread25 = ["M10x1.25", '1/8"', "M6x0.75"];
-var d32 = [12, 19.5, 22, 40, 71, 113, 11, 10];
-var thread32 = ["M10x1.25", '1/8"', "M8x1.0"];
-var d40 = [16, 27, 30, 50, 78, 130, 12, 10];
-var thread40 = ["M14x1.5", '1/8"', "M10x1.25"]; 
-var d50 = [20, 32, 35, 58, 90, 150, 13, 12];
-var thread50 = ["M18x1.5", '1/4"', "M12x1.25"];
+var smcThreads = [["M8x1.25", '1/8"', "M5x0.8"],
+["M10x1.25", '1/8"', "M6x0.75"],
+["M10x1.25", '1/8"', "M8x1.0"],
+["M14x1.5", '1/8"', "M10x1.25"],
+["M18x1.5", '1/4"', "M12x1.25"],
+["M18x1.5", '1/4"', "M14x1.5"],
+["M22x1.5", '3/8"', "N/A"],
+["M26x1.5", '1/2"', "N/A"]
+];
+var smcLimit = document.getElementsByClassName("smc").length;
+var boreLimit = document.getElementsByClassName("boreSize").length;
+var threadLimit = document.getElementsByClassName("smcThread").length;
 
-var limit = document.getElementsByClassName("smc").length;
-
-function autoFill(){
-  var input = document.getElementById("bore").value;
-  if (input === "20"){
-    for(i = 0; i < limit; i++){
-      document.getElementsByClassName("smc")[i].value = d20[i];
-    }
-    for(i = 0; i < 3; i++) {
-      document.getElementsByClassName("smcThread")[i].value = thread20[i];
-    }
-  }
-  if (input === "25"){
-    for(i = 0; i < limit; i++){
-      document.getElementsByClassName("smc")[i].value = d25[i];
-    }
-    for(i = 0; i < 3; i++) {
-      document.getElementsByClassName("smcThread")[i].value = thread25[i];
-    }
-  }
-  if (input === "32"){
-    for(i = 0; i < limit; i++){
-      document.getElementsByClassName("smc")[i].value = d32[i];
-    }
-    for(i = 0; i < 3; i++) {
-      document.getElementsByClassName("smcThread")[i].value = thread32[i];
-    }
-  }
-  if (input === "40"){
-    for(i = 0; i < limit; i++){
-      document.getElementsByClassName("smc")[i].value = d40[i];
-    }
-    for(i = 0; i < 3; i++) {
-      document.getElementsByClassName("smcThread")[i].value = thread40[i];
-    }
-  }
-  if (input === "50"){
-    for(i = 0; i < limit; i++){
-      document.getElementsByClassName("smc")[i].value = d50[i];
-    }
-    for(i = 0; i < 3; i++) {
-      document.getElementsByClassName("smcThread")[i].value = thread50[i];
+function autoFill() {
+  var bore = document.getElementById("bore").value;
+  for (i = 0; i < boreLimit; i++) {
+    if (bore === boreSizes[i]) {
+      for (j = 0; j < smcLimit; j++) {
+        document.getElementsByClassName("smc")[j].value = smcMeasures[i][j];
+      }
+      for (j = 0; j < threadLimit; j++) {
+        document.getElementsByClassName("smcThread")[j].value = smcThreads[i][j];
+      }
     }
   }
 }
