d24f21acfcaedd2203fe9a2d4f86305433629db4
Jaime Yerena <ing.carlosjaime@outlook.com>
2020-05-11 22:56:52 -0500

    Long and short stroke measures complete for CG1 autofill function

--- cg1.js
+++ cg1.js
@@ -43,7 +43,11 @@ var threadLimit = document.getElementsByClassName("smcThread").length;
 var stroke = document.getElementById("stroke");
 
 function longStrokeMeasure() {
-  if (Number(stroke.value) > 300) {
+  if (Number(stroke.value) > 200 && document.getElementById("bore").value === "20"){
+    for(j = 4; j < smcLimit; j++) {
+      smcMeasures[0][j] = longStroke[0][j-4];
+    }
+  } else if (Number(stroke.value) > 300) {
     for(i = 0; i < boreSizes.length; i++) {
       for(j = 4; j < boreSizes.length; j++) {
         smcMeasures[i][j] = longStroke[i][j-4];
